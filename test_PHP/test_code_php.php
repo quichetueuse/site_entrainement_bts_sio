8b9aaff97b9ebfee82dd0a6ce83ae43bdf8b76e9
quichetueuse <eliot.barrabes@gmail.com>
2023-11-29 08:46:27 +0100

    ajout tp 1 sql css

---
+++ test_PHP/test_code_php.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+<?php
+echo "The current date is ";
+echo date("l F d, Y ");
+echo "<br>ABC";
+echo "<p style='font-weight: bold; color: #3388CC; font-size: 25px; text-align: center;'>This is a big text</p>";
+?>
+</body>
+</html>
