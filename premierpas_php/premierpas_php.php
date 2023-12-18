03ad655e2d3f575253d08a1a6259a548ef90816f
quichetueuse <eliot.barrabes@gmail.com>
2023-12-18 14:04:07 +0100

    premier pas php

---
+++ premierpas_php/premierpas_php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Correction</title>
+</head>
+<body>
+
+
+
+</body>
+
+</html>
+
+
+<?php $connexion = 2;
+echo $connexion;
